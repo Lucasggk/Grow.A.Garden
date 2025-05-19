695759ebc8c45211db1d3dc4f8b329ddf41d532e
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-19 13:36:24 -0300

    Update Principal.loader.lua

--- Principal.loader.lua
+++ Principal.loader.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/df0359e8a230912f5cf76c6d7edc9cab743fe9dc/Loja.lua"))()
-
+local values = {"Commun", "Uncommun", "Rare", "Legendery", "Mythical", "Divine", "Prismatic"}
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
  
@@ -25,11 +25,11 @@ local section = loja:AddSection("Seeds (select by rarity)")
 
 local Dropdown = loja:AddDropdown("", {
     Title = "auto buy seeds",
-    Values = {"Commun", "Uncommun", "Rare", "Legendery", "Mythical", "Divine", "Prismatic"},
+    Values = values,
     Multi = true,
-    Default = Values
+    Default = values
 })
 
 Dropdown:OnChanged(function(value)
- print(value) 
+    print(value)
 end)
