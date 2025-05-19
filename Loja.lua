046b626ef03e57fa270aed68671e8a692f4f1252
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-19 12:15:41 -0300

    Update Loja.lua

--- Loja.lua
+++ Loja.lua
@@ -1,4 +1,7 @@
 local commun = {"Carrot", "Strawberry"}
 local uncommun = {"Blueberry", "Orange Tulip"}
-
-
+local rare = {"Tomato", "Corn", "Daffodil"}
+local legendery = {"Watermelon", "Pumpkin", "Apple", "Bamboo"}
+local mythical = {"Coconut", "Cactus", "Dragon Fruit", "Mango"}
+local divine = {"Grape", "Mushroom", "Pepper", "Cacao"}
+local prismatic = {"Beanstalk"}
