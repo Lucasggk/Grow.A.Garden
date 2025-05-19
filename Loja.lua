1ab35f94670d48b88eb1cdcad0b0069fb65437e2
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-19 13:17:55 -0300

    Update Loja.lua

--- Loja.lua
+++ Loja.lua
@@ -1,8 +1,9 @@
-local commun = {"Carrot", "Strawberry"}
-local uncommun = {"Blueberry", "Orange Tulip"}
-local rare = {"Tomato", "Corn", "Daffodil"}
-local legendery = {"Watermelon", "Pumpkin", "Apple", "Bamboo"}
-local mythical = {"Coconut", "Cactus", "Dragon Fruit", "Mango"}
-local divine = {"Grape", "Mushroom", "Pepper", "Cacao"}
-local prismatic = {"Beanstalk"}
-local gear = {"Watering Can", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Lightning Rod", "Master Sprinkler", "Favorite Tool"}
+commun = {"Carrot", "Strawberry"}
+uncommun = {"Blueberry", "Orange Tulip"}
+rare = {"Tomato", "Corn", "Daffodil"}
+legendery = {"Watermelon", "Pumpkin", "Apple", "Bamboo"}
+mythical = {"Coconut", "Cactus", "Dragon Fruit", "Mango"}
+divine = {"Grape", "Mushroom", "Pepper", "Cacao"}
+prismatic = {"Beanstalk"}
+gear = {"Watering Can", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Lightning Rod", "Master Sprinkler", "Favorite Tool"}
+
