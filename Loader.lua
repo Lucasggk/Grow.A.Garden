98895151dffe940eb9344c33aeb9bf5d7c350eb9
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-20 21:16:07 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,16 +2,19 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Plant = ReplicatedStorage.GameEvents.Plant_RE
 local fruta = g().fruta
 local posi = g().posi
-local startPos = Vector3.new(posi)
-local endPos = Vector3.new(posi)
-local step = 0.001 -- distância mínima entre plantações (muito pequeno)
+
+-- Mantém a altura (Y) e faz variação de 0.5 studs para os lados no eixo X
+local startPos = Vector3.new(posi.X - 0.5, posi.Y, posi.Z)
+local endPos = Vector3.new(posi.X + 0.5, posi.Y, posi.Z)
+
+local step = 0.0005
 local direction = (endPos - startPos).Unit
 local distance = (endPos - startPos).Magnitude
 
 for i = 0, distance, step do
     local pos = startPos + direction * i
     Plant:FireServer(pos, fruta)
-    task.wait(0.0001) -- sem delay pra plantar rápido
+    task.wait(0.0001)
 end
 
-task.wait(0) -- mini delay final
+task.wait(0)
