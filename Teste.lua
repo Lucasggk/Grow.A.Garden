bf9c48391cf6ff24649f2537ff473a17ed6aff6f
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-24 16:07:34 -0300

    Update Teste.lua

--- Teste.lua
+++ Teste.lua
@@ -1,11 +1,12 @@
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
- 
-local Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()
 
 local Show_Button = false -- Shows the button for toggle fluent ui manually. If "false", works only on mobile, if "true", works everytime.
 local Button_Icon = "rbxassetid://10734897102" -- Icon of the button for toggle fluent ui.
 local Button_Transparency = 1 -- Transparency of button background (check line #6365).
+
+
+local Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()
 
 local Window = Fluent:CreateWindow({
     Title = "Grow a Garden | ",
