cffa39b4687bc6416d49f006913ec1b709a69009
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-24 16:10:27 -0300

    Update Teste.lua

--- Teste.lua
+++ Teste.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 
 local Show_Button = false -- Shows the button for toggle fluent ui manually. If "false", works only on mobile, if "true", works everytime.
-local Button_Icon = "rbxassetid://10734897102" -- Icon of the button for toggle fluent ui.
+local Button_Icon = "rbxassetid://94979807066345" -- Icon of the button for toggle fluent ui.
 local Button_Transparency = 1 -- Transparency of button background (check line #6365).
 
 
