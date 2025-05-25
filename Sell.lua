297c54e9e81feccb6df9ab665e0bc045dc955322
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-25 11:06:45 -0300

    Update Sell.lua

--- Sell.lua
+++ Sell.lua
@@ -1,3 +1,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local sellfruits = ReplicatedStorage:WaitForChild("GameEvents"):WaitForChild("Sell_Inventory"):FireServer()
 local sellmoons = game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("NightQuestRemoteEvent"):FireServer(unpack("SubmitAllPlants"))
+local hrp = character:WaitForChild("HumanoidRootPart")
+
+function savepos()
+  Vector3.new(hrp.Position.X, hrp.Position.Y, hrp.Position.Z)
