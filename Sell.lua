149ea9d21c59fc27894cc955ab60b1ceccf87234
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-25 11:15:10 -0300

    Update Sell.lua

--- Sell.lua
+++ Sell.lua
@@ -1,13 +1,16 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local sellfruits = ReplicatedStorage:WaitForChild("GameEvents"):WaitForChild("Sell_Inventory"):FireServer()
-local sellmoons = ReplicatedStorage:WaitForChild("GameEvents"):WaitForChild("NightQuestRemoteEvent"):FireServer(unpack("SubmitAllPlants"))
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
 local hrp = character:WaitForChild("HumanoidRootPart")
-local tps = game:GetService("TeleportService")
+
+local sellf = ReplicatedStorage:WaitForChild("GameEvents"):WaitForChild("Sell_Inventory"):FireServer()
+local sellm = ReplicatedStorage:WaitForChild("GameEvents"):WaitForChild("NightQuestRemoteEvent"):FireServer("SubmitAllPlants")
 
 function savepos()
-  Pos = Vector3.new(hrp.Position.X, hrp.Position.Y, hrp.Position.Z)
+    Pos = Vector3.new(hrp.Position.X, hrp.Position.Y, hrp.Position.Z)
 end
 
-function tps(pos)
-  tps(pos)
+function teleportTo(pos)
+    hrp.CFrame = CFrame.new(pos)
 end
