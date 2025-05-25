7870be7c00283ddf54a376ce2b4542d13c2972a1
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-25 20:22:53 -0300

    Update and rename Kaitun.luau to Kaitun.lua

---
+++ Kaitun.lua
@@ -0,0 +1,16 @@
+-- locais exenciais 
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local hrp = char:WaitForChild("HumanoidRootPart")
+
+-- locais do script game
+
+
+
+-- locais de local
+
+local nseed = {"Carrot", "Strawberry", "Blueberry", "Orange Tulip", "Tomato", "Corn", "Daffodil", "Watermelon", "Pumpkin", "Apple", "Bamboo", "Coconut", "Cactus", "Dragon Fruit", "Mango", "Grape", "Mushroom", "Pepper", "Cacao", "Beanstalk"}
+
+-- locais de funções 
