a1370356c8785d0cbdf8cf5315e1f714feeeda63
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-26 12:53:11 -0300

    Update and rename Candy blossom.lua to Easter stock.lua

---
+++ Easter stock.lua
@@ -0,0 +1,21 @@
+local Candy = game:GetService("ReplicatedStorage").GameEvents.BuyEasterStock
+
+stock = {
+    "Chocolate Carrot Seed",   -- Comum
+    "Red Lollipop",            -- Incomum
+    "Candy Sunflower",         -- Raro
+    "Easter Egg",              -- Lendário
+    "Chocolate Sprinkler",     -- Mítico
+    "Candy Blossom"            -- Divino
+}
+
+if game.PlaceId == 12688469563 then 
+while true do 
+    for _, candy in ipairs(stock) do
+     Candy:FireServer(candy)
+    end 
+    task.wait(0.1)
+  end
+else
+  print("jogo errado: ".. game.PlaceId .."jogo correto: 12688469563")
+end
