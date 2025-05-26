73315b3d641c2cc2bfbaafd77adf326fae576f23
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-26 12:55:41 -0300

    Delete Loja.lua

--- Loja.lua
+++
@@ -1,9 +0,0 @@
-commun = {"Carrot", "Strawberry"}
-uncommun = {"Blueberry", "Orange Tulip"}
-rare = {"Tomato", "Corn", "Daffodil"}
-legendery = {"Watermelon", "Pumpkin", "Apple", "Bamboo"}
-mythical = {"Coconut", "Cactus", "Dragon Fruit", "Mango"}
-divine = {"Grape", "Mushroom", "Pepper", "Cacao"}
-prismatic = {"Beanstalk"}
-gear = {"Watering Can", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Lightning Rod", "Master Sprinkler", "Favorite Tool"}
-
