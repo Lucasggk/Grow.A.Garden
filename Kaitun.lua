2a7b911f9f8ccdd14cde720e1f0b7d119b3d6b0e
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-25 22:00:19 -0300

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -39,35 +39,35 @@ end
 
 -- local de verificação 
 
--- Verificação de lote atual
+
 if farm == lote1 then
-  plan_lote = vector.create()
+  plan_lote = Vector3.new()
   tp_lote = 33.371002197265625, 2.999999761581421, -64.72699737548828
   print("Lote atual: 1")
 
 elseif farm == lote2 then
-  plan_lote = vector.create()
+  plan_lote = Vector3.new()
   tp_lote = -101.6290054321289, 3, -64.72621154785156
   print("Lote atual: 2")
 
 elseif farm == lote3 then
-  plan_lote = vector.create()
+  plan_lote = Vector3.new()
   tp_lote = -235.37002563476562, 2.999999761581421, -61.78000259399414
   print("Lote atual: 3")
 
 elseif farm == lote4 then
-  plan_lote = vector.create()
+  plan_lote = Vector3.new()
   tp_lote = 34.629005432128906, 2.999999761581421, 36.78675842285156
   print("Lote atual: 4")
 
 elseif farm == lote5 then
-  plan_lote = vector.create()
+  plan_lote = Vector3.new()
   tp_lote = -100.3709945678711, 2.999999761581421, 36.793582916259766
   print("Lote atual: 5")
+
 else
   print("hop server")
 end
-
 
 
 
