98d38d134aa168d290cd9847f6d29459d3db8ec4
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-25 22:23:03 -0300

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -41,33 +41,32 @@ end
 
 
 if farm == lote1 then
-  plan_lote = Vector3.new()
+  plan_lote = Vector3.new(42.18696975708008, 0.13552513718605042, -76.71431732177734)
   tp_lote = 33.371002197265625, 2.999999761581421, -64.72699737548828
   print("Lote atual: 1")
 
 elseif farm == lote2 then
-  plan_lote = Vector3.new()
+  plan_lote = Vector3.new(-91.24068450927734, 0.13552513718605042, -76.4989242553711)
   tp_lote = -101.6290054321289, 3, -64.72621154785156
   print("Lote atual: 2")
 
 elseif farm == lote3 then
-  plan_lote = Vector3.new()
+  plan_lote = Vector3.new(-227.4713897705078, 0.13552513718605042, -77.53561401367188)
   tp_lote = -235.37002563476562, 2.999999761581421, -61.78000259399414
   print("Lote atual: 3")
 
 elseif farm == lote4 then
-  plan_lote = Vector3.new()
+  plan_lote = Vector3.new(-227.4713897705078, 0.13552513718605042, -77.53561401367188)
   tp_lote = 34.629005432128906, 2.999999761581421, 36.78675842285156
   print("Lote atual: 4")
 
 elseif farm == lote5 then
-  plan_lote = Vector3.new()
+  plan_lote = Vector3.new(25.35358428955078, 0.13552513718605042, 49.66905212402344)
   tp_lote = -100.3709945678711, 2.999999761581421, 36.793582916259766
   print("Lote atual: 5")
 
 else
   print("hop server")
 end
-
 
 
