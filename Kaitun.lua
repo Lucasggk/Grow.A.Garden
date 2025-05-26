2bd477af774549ddc5f4c4914a19b76194c51d7c
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-25 21:08:48 -0300

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -18,12 +18,21 @@ local nseed = {"Carrot", "Strawberry", "Blueberry", "Orange Tulip", "Tomato", "C
 local farm = hrp.Position
 local pos = hrp.CFrame
 
+local lote1
+local lote2
+local lote3
+local lote4 = {34.629005432128906, 2.999999761581421, 36.78675842285156}
+local lote5
+local lote6
+
+local plant_lote
+
 -- locais de funções 
 
 function tp(localizacao)
   hrp.CFrame = CFrame.new(localizacao)
 end
 
---
+-- local de verificação 
 
 
