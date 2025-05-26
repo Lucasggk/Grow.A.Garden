2f2eb132fb26d1350ef092c7c419e7e5031683f5
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-26 05:33:11 -0300

    Update Auto buy.lua

--- Auto buy.lua
+++ Auto buy.lua
@@ -8,5 +8,36 @@ local buySeed = ReplicatedStorage.GameEvents.BuySeedStock
 local buyGear = ReplicatedStorage.GameEvents.BuyGearStock
 local buyMoon = ReplicatedStorage.GameEvents.BuyEventShopStock
 local buymoon2 = ReplicatedStorage.GameEvents.BuyNightEventShopStock
-local BuyPet = ReplicatedStorage.GameEvents.BuyPetEgg
+local buyPet = ReplicatedStorage.GameEvents.BuyPetEgg
+
+local seed = {"Bamboo", "Coconut", "Cactus", "Dragon Fruit", "Mango", "Grape", "Mushroom", "Pepper", "Cacao", "Beanstalk"}
+local moon = { "Night Seed Pack", "Night Egg", "Blood Banana", "Moon Melon", "Star Caller", "Blood Owl"}
+local moon2 = {"Night Egg", "Night Seed Pack", "Star Caller", "Celestiberry"}
+local gear = {"Watering Can", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Lightning Rod", "Master Sprinkler"}
+local pet = {1, 2, 3}
+
+function buy()
+  for i = 1, 50 do
+    for _, se in ipairs(seed) do
+      buySeed:FireServer(se)
+    end 
+    for _, ge in ipairs(gear) do
+      buyGear:FireServer(ge)
+    end 
+    for _, mo in ipairs(moon) do
+      buyMoon:FireServer(mo)
+    end 
+    for _, mo2 in ipairs(moon2) do
+      buyMoon2:FireServer(mo2)
+    end  
+    for _, pe in ipairs(pet) do 
+            buyPet:FireServer(pe)
+    end
+    task.wait()
+  end
+end
+
+
+
+
 
