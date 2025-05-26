924aa58c36371aa320467ae5e9802ca78343795e
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-26 05:21:46 -0300

    Update and rename Sell.lua to Auto buy.lua

---
+++ Auto buy.lua
@@ -0,0 +1,11 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local buySeed = ReplicatedStorage.GameEvents.BuySeedStock
+local buyGear = ReplicatedStorage.GameEvents.BuyGearStock
+local buyMoon = ReplicatedStorage.GameEvents.BuyEventShopStock
+
+local BuyPet = ReplicatedStorage.GameEvents.BuyPetEgg
