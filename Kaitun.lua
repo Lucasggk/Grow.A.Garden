472f9e6f2e5650d121bff5d5b2e3a8428838a183
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-25 23:31:34 -0300

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -90,27 +90,32 @@ end
 -- local de verificação 
 
 
-if farm == lote1 then
+local function isNear(vec1, vec2, tolerance)
+    tolerance = tolerance or 1 
+    return (vec1 - vec2).Magnitude <= tolerance
+end
+
+if isNear(farm, lote1, 1) then
   plan_lote = Vector3.new(42.18696975708008, 0.13552513718605042, -76.71431732177734)
   tp_lote = 33.371002197265625, 2.999999761581421, -64.72699737548828
   print("Lote atual: 1")
 
-elseif farm == lote2 then
+elseif isNear(farm, lote2, 1) then
   plan_lote = Vector3.new(-91.24068450927734, 0.13552513718605042, -76.4989242553711)
   tp_lote = -101.6290054321289, 3, -64.72621154785156
   print("Lote atual: 2")
 
-elseif farm == lote3 then
+elseif isNear(farm, lote3, 1) then
   plan_lote = Vector3.new(-227.4713897705078, 0.13552513718605042, -77.53561401367188)
   tp_lote = -235.37002563476562, 2.999999761581421, -61.78000259399414
   print("Lote atual: 3")
 
-elseif farm == lote4 then
+elseif isNear(farm, lote4, 1) then
   plan_lote = Vector3.new(-227.4713897705078, 0.13552513718605042, -77.53561401367188)
   tp_lote = 34.629005432128906, 2.999999761581421, 36.78675842285156
   print("Lote atual: 4")
 
-elseif farm == lote5 then
+elseif isNear(farm, lote5, 1) then
   plan_lote = Vector3.new(25.35358428955078, 0.13552513718605042, 49.66905212402344)
   tp_lote = -100.3709945678711, 2.999999761581421, 36.793582916259766
   print("Lote atual: 5")
