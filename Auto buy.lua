de467bdd596ad856ebf5e869c512daef0f17ca6d
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-26 21:21:33 -0300

    Update Auto buy.lua

--- Auto buy.lua
+++ Auto buy.lua
@@ -1,1 +1,53 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local buySeed = ReplicatedStorage.GameEvents.BuySeedStock
+local buyGear = ReplicatedStorage.GameEvents.BuyGearStock
+local buyMoon = ReplicatedStorage.GameEvents.BuyEventShopStock
+local buymoon2 = ReplicatedStorage.GameEvents.BuyNightEventShopStock
+local buyPet = ReplicatedStorage.GameEvents.BuyPetEgg
 
+local seed = {"Carrot"}
+local gear = {"Watering Can", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Lightning Rod", "Master Sprinkler"}
+local bloodlit = {}
+local moonlit = {}
+local pet = {1, 2, 3}
+
+function s() 
+  for _, sd in ipairs(seed) do
+    buySeed:FireServer(sd)
+  end
+end
+
+function g() 
+  for _, gr in ipairs(gear) do
+    buyGear:FireServer(gr)
+  end
+end
+
+function b() 
+  for _, bl in ipairs(bloodlit) do
+    buyMoon:FireServer(bl)
+  end
+end
+
+function m() 
+  for _, ml in ipairs(moonlit) do
+    buyMoon2:FireServer(ml)
+  end
+end
+
+function p() 
+  for _, p in ipairs(pet) do
+    buyPet:FireServer(p)
+  end
+end
+
+function buyall()
+  for i = 1, 50 do
+    s()
+    g()
+    b()
+    m()
+    p()
+    
+  end
+end
