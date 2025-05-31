e57a5491bbb9ea2ee8cb1ab1b1f337b2a2cca4d0
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-31 19:15:47 -0300

    Update Buy night.lua

--- Buy night.lua
+++ Buy night.lua
@@ -1,1 +1,8 @@
-
+local buyMoon = ReplicatedStorage.GameEvents.BuyEventShopStock
+local buymoon2 = ReplicatedStorage.GameEvents.BuyNightEventShopStock
+local byallmoon = {"Mysterious Crate", "Night Seed Pack", "Night Egg", "Blood Banana", "Moon Melon", "Star Caller", "Blood Hedgehog", "Blood Kiwi", "Blood Owl"}
+local byallmoon2 = {"Night Egg", "Night Seed Pack", "Twilight Crate", "Star Caller", "Moon Cat", "Celestiberry", "Moon Mango"}
+local bsm = false
+local bsm2 = false
+local selectedMoons = {}
+local selectedMoons2 = {}
