f2c01df45de3ef084f1cd5c9fbc13833ee56d305
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-03 17:33:41 -0300

    Update Script version.lua

--- Script version.lua
+++ Script version.lua
@@ -1,11 +1,10 @@
-local script_version = {
-    version = "",
-    alpha = ""
-}
-if script_version.alpha == true then
-    script_version.alpha = "Alpha version"
-else
-    script_version.alpha = "Release version"
+return function(script_version)
+    if script_version.alpha == true then
+        script_version.alpha = "Alpha version"
+    else
+        script_version.alpha = "Release version"
+    end
+    local vful = script_version.version .. " - " .. script_version.alpha
+    print("Versão completa:", vful)
+    return vful
 end
-local vful = script_version.version .." - ".. script_version.alpha
-
