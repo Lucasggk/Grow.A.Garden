21dc3ccec9a3096475ed158b5a3b91f792ed00c6
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-03 19:34:15 -0300

    Update Script version.lua

--- Script version.lua
+++ Script version.lua
@@ -2,6 +2,7 @@ function sc()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/main/Teste.lua"))()
 end
 
+Fluent:Destroy()
 sc()
 task.wait(0.1)
 Fluent:Destroy()
