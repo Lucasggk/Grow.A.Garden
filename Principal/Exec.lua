614999ffae3d082e688cf2766234c3eac4a63d79
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-04 14:05:56 -0300

    Update Exec.lua

--- Principal/Exec.lua
+++ Principal/Exec.lua
@@ -1,5 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Principal/Lucas%20hub.lua"))()
-task.wait(0.1)
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()task.wait(0.1)
 Fluent:Destroy()
 task.wait(0.1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Principal/Lucas%20hub.lua"))()
