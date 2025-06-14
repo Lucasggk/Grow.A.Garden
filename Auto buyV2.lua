9139853b8940e965bf5fc7c1804cf3f10c8fdd69
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-14 11:33:04 -0300

    Update Auto buyV2.lua

--- Auto buyV2.lua
+++ Auto buyV2.lua
@@ -43,7 +43,7 @@ local gearScroll = player.PlayerGui.Gear_Shop.Frame.ScrollingFrame
 task.spawn(function()
     while true do
         for _, item in ipairs(gearScroll:GetChildren()) do
-            if item:IsA("Frame") and not table.find({ "Favorite Tool", "Harvest Tool", "Friendship Pot" }, item.Name) then
+            if item:IsA("Frame") and not table.find({ "Cleaning Spray", "Favorite Tool", "Harvest Tool", "Friendship Pot" }, item.Name) then
                 local stock = item:FindFirstChild("Main_Frame") and item.Main_Frame:FindFirstChild("Stock_Text")
                 if stock and stock:IsA("TextLabel") and stock.Text ~= "X0 Stock" then
                     rs.GameEvents.BuyGearStock:FireServer(item.Name)
