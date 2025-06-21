ae54925d5b5b38ab3a56b93cd77a2e3abd636000
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-21 11:26:50 -0300

    Create Auto buy v1.lua

---
+++ Loja/Newnames.lua/Auto buy v1.lua
@@ -0,0 +1,5 @@
+local byallseed = {
+    "Carrot", "Strawberry", "Blueberry", "Tomato", "Cauliflower", "Watermelon", "Green Apple",
+    "Avocado", "Banana", "Pineapple", "Kiwi", "Bell Pepper", "Prickly Pear", "Loquat",
+    "Feijoa", "Sugar Apple"
+}
