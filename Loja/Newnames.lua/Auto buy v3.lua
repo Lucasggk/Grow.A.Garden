69b02bcc0895b374458fba1e60575b6c8a951a7a
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-21 11:27:04 -0300

    Create Auto buy v3.lua

---
+++ Loja/Newnames.lua/Auto buy v3.lua
@@ -0,0 +1,33 @@
+local ignoreNames = {
+    -- Sementes
+    ["Carrot"] = true,
+    ["Strawberry"] = true,
+    ["Blueberry"] = true,
+    ["Tomato"] = true,
+    ["Cauliflower"] = true,
+    ["Watermelon"] = true,
+    ["Green Apple"] = true,
+    ["Avocado"] = true,
+    ["Banana"] = true,
+    ["Pineapple"] = true,
+    ["Kiwi"] = true,
+    ["Bell Pepper"] = true,
+    ["Prickly Pear"] = true,
+    ["Loquat"] = true,
+    ["Feijoa"] = true,
+    ["Sugar Apple"] = true,
+
+    -- Gears
+    ["Watering Can"] = true,
+    ["Trowel"] = true,
+    ["Recall Wrench"] = true,
+    ["Basic Sprinkler"] = true,
+    ["Advanced Sprinkler"] = true,
+    ["Godly Sprinkler"] = true,
+    ["Lightning Rod"] = true,
+    ["Master Sprinkler"] = true,
+    ["Cleaning Spray"] = false,
+    ["Favorite Tool"] = false,
+    ["Harvest Tool"] = false,
+    ["Friendship Pot"] = false,
+}
