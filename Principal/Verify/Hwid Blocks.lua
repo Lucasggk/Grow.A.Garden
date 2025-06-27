da8e95abef93a13d12b59d09c23062469148134f
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-27 17:43:15 -0300

    Update Hwid Blocks.lua

--- Principal/Verify/Hwid Blocks.lua
+++ Principal/Verify/Hwid Blocks.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/main/Principal/Verify/SendWebhook_verify.lua"))()
 local hw = {
   [""] = true,
   [""] = true,
@@ -9,7 +10,7 @@ Você não tem permissão para usar o script!
 Dono do script já foi avisado!
 ]]
 
-local hwp = get_hwid()
+local hwp = get_hwid()  
 
 if hw[hwp] then
   game.Players.LocalPlayer:Kick(msg)
