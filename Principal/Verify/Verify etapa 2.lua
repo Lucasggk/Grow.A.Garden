5ea41392413c2b8e2c87d3b1c93e6815fbe9e0a9
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-28 17:39:36 -0300

    Update Verify etapa 2.lua

--- Principal/Verify/Verify etapa 2.lua
+++ Principal/Verify/Verify etapa 2.lua
@@ -6,6 +6,12 @@ function kick(text)
     game.Players.LocalPlayer:Kick(text)
 end
 
+function lp(n)
+    for i = 1, n do
+        print("")
+    end
+end
+
 local block_hwid = {
     ["Nada ainda"] = true
 }
@@ -20,14 +26,13 @@ local miguel = {
     Hwid = "07f39de0a0266ebdefec9bb2cb40d2b1af692211ee017b166eb0af43b27834f2bd24eaf36a73df9838203600d9eee4db",
 }
 
-
-
 if (miguel.Id == id) and (miguel.Exec == exec) and (miguel.Hwid == hwid) then
     print("Fazendo Outras Verificações...")
     task.wait(0.1)
     print("Usuário Miguel verificado.")
     print("Executando...")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/main/Principal/Lucas%20hub.lua"))()
+    lp(5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/main/Principal/Lucas%20hub.lua"))() 
 else 
     kick(".")
 end
