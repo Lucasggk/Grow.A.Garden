b4a6bf8626a492d310be293a71430e606bb31a08
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-28 16:58:43 -0300

    Update Verify etapa 2.lua

--- Principal/Verify/Verify etapa 2.lua
+++ Principal/Verify/Verify etapa 2.lua
@@ -1,28 +1,29 @@
 local hwid = (get_hwid and get_hwid()) or (gethwid and gethwid())
-local id = tostring(game.Players.LocalPlayer.UserId)
+local id = game.Players.LocalPlayer.UserId
 local exec = identifyexecutor()
 
 function kick(text)
-  game.Players.LocalPlayer:Kick(text)
+    game.Players.LocalPlayer:Kick(text)
 end
 
 local block_hwid = {
-  ["Nada ainda"] = true
+    ["Nada ainda"] = true
 }
 
 if block_hwid[hwid] then 
-  kick("Hwid blocked")
+    kick("Hwid blocked")
 end
 
 local miguel = {
-  Id = "2000903030",
-  Exec = "Krnl",
-  Hwid = "07f39de0a0266ebdefec9bb2cb40d2b1af692211ee017b166eb0af43b27834f2bd24eaf36a73df9838203600d9eee4db",
+    Id = 2000903030,
+    Exec = "Krnl",
+    Hwid = "07f39de0a0266ebdefec9bb2cb40d2b1af692211ee017b166eb0af43b27834f2bd24eaf36a73df9838203600d9eee4db",
 }
 
 if (miguel.Id == id) and (miguel.Exec == exec) and (miguel.Hwid == hwid) then
-  Print("Fazendo Outras Verificações...")
-  task.wait(0.1)
-  print("Usuário Miguel verificado.")
-  
+    print("Fazendo Outras Verificações...")
+    task.wait(0.1)
+    print("Usuário Miguel verificado.")
+    print("Executando...")
+    
 end
