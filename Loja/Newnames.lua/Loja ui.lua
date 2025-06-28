a46a8680334a2ce28a02295ff1ee49f38b7b8081
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-28 16:16:55 -0300

    Update Loja ui.lua

--- Loja/Newnames.lua/Loja ui.lua
+++ Loja/Newnames.lua/Loja ui.lua
@@ -1,5 +1,5 @@
 local byallseed = {
-    "Carrot", "Strawberry", "Blueberry", "Tomato", "Cauliflower", "Watermelon", "Green Apple",
+    "Carrot", "Strawberry", "Blueberry", "Tomato", "Cauliflower", "Watermelon", "Rafflesia", "Green Apple",
     "Avocado", "Banana", "Pineapple", "Kiwi", "Bell Pepper", "Prickly Pear", "Loquat",
     "Feijoa", "Pitcher Plant", "Sugar Apple"
 }
