04bbab5bf3bb0397e587da4d9eb2d82d1527d22c
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-06-30 20:44:49 -0300

    Update Lucas hub.lua

--- Principal/Lucas hub.lua
+++ Principal/Lucas hub.lua
@@ -273,34 +273,55 @@ dropdownGear:OnChanged(function(Value)
     end
 end)
 
-local section = loja:AddSection("Pets buy")
+local section = loja:AddSection("Auto Buy egg")
 
-loja:AddParagraph({
-    Title = "Compra todos Eggs presentes na loja",
-    Content = ""
+function gne(n)
+    local f, l, r = workspace.NPCS["Pet Stand"].EggLocations:GetChildren(), {}, {}
+    for _, v in ipairs(f) do if v.Name:lower():find("egg") then l[#l+1] = v.Name end end
+    n = n:lower()
+    for i = 1, #l do if l[i]:lower() == n then r[#r+1] = i end end
+    return r
+end
+local selected = {}
+local mdp = loja:AddDropdown("MultiDropdown", {
+    Title = "Selecionar Eggs",
+    Description = "",
+    Values = {
+        "Common Egg", "Common Summer Egg", "Rare Summer Egg",
+        "Mythical Egg", "Paradise Egg", "Bee Egg", "Bug Egg"
+    },
+    Multi = true,
+    Default = {},
 })
+mdp:OnChanged(function(Value)
+    selected = {}
+    for name, state in next, Value do
+        selected[#selected+1] = name
+    end
+end)
 
-loja:AddButton({
-        Title = "comprar todos Eggs Na loja Atual",
-        Description = "auto se explica",
-        Callback = function()
-            buypetegg()
-        end
-    })
+_G.bpd = false
 
 loja:AddToggle("", {
-        Title = "comprar todos Eggs Na loja Atual Automaticamente",
-        Description = "auto se explica",
-        Default = false,
-        Callback = function(value)
-         bsp = value
-        end
-    })
-
-loja:AddParagraph({
-    Title = "Compra todos Eggs escolhidos no Dropdown.",
-    Content = ""
+    Title = "Comprar Automaticamente Eggs selecionados. \n",
+    Description = "",
+    Default = false,
+    Callback = function(state)
+        _G.bpd = state
+        task.spawn(function()
+            while _G.buying do
+                for _, name in ipairs(selected) do
+                    local indices = gne(name)
+                    for _, i in ipairs(indices) do
+                        BuyPet:FireServer(i)
+                    end
+                end
+                task.wait(1)
+            end
+        end)
+    end
 })
+
 
 
 
