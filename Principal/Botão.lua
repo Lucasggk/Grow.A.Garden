00b9bf8af9aa3e4dd65799f6f9d9695d7b61e4f3
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-03 14:38:54 -0300

    Update Botão.lua

--- Principal/Botão.lua
+++ Principal/Botão.lua
@@ -46,5 +46,7 @@ game:GetService("UserInputService").InputChanged:Connect(function(input)
 end)
 
 imageButton.MouseButton1Click:Connect(function()
-	Window:Minimize()
+	if _G.FluentWindow then
+		_G.FluentWindow:Minimize()
+	end
 end)
