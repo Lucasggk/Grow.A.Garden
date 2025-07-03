8f18fda0d5887ed796bef2340ac4c86ca43dd140
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-03 14:23:24 -0300

    Update Lucas hub.lua

--- Principal/Lucas hub.lua
+++ Principal/Lucas hub.lua
@@ -39,9 +39,9 @@ player.CharacterAdded:Connect(function(char)
     hrp = character:WaitForChild("HumanoidRootPart")
 end)
 
-getgenv().Show_Button = false
+
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/Beta.lua"))() 
-getgenv().Show_Button = false
+
 
 local Window = Fluent:CreateWindow({
     Title = "Grow a Garden |",
@@ -959,9 +959,6 @@ task.spawn(function()
     end
 end)
 
-task.wait(10)
-Window:Minimize()
-
 task.spawn(function()
     local player = game:GetService("Players").LocalPlayer
     player.Idled:Connect(function()
@@ -974,55 +971,4 @@ end)
 
 
 
-
-local player = game.Players.LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")
-
-local decalId = "rbxassetid://119963300684494"
-
-local screenGui = Instance.new("ScreenGui")
-screenGui.Name = "DraggableImageButtonGui"
-screenGui.ResetOnSpawn = false
-screenGui.Parent = playerGui
-
-local imageButton = Instance.new("ImageButton")
-imageButton.Name = "DraggableButton"
-imageButton.Image = decalId
-imageButton.Size = UDim2.new(0, 60, 0, 60)
-imageButton.Position = UDim2.new(0.5, -50, 0.5, -50)
-imageButton.AnchorPoint = Vector2.new(0.5, 0.5)
-imageButton.BackgroundTransparency = 0
-imageButton.AutoButtonColor = false
-imageButton.Parent = screenGui
-
-local dragging, dragInput, mousePos, buttonPos = false
-
-imageButton.InputBegan:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-		dragging, mousePos, buttonPos = true, input.Position, imageButton.Position
-		input.Changed:Connect(function()
-			if input.UserInputState == Enum.UserInputState.End then dragging = false end
-		end)
-	end
-end)
-
-imageButton.InputChanged:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-		dragInput = input
-	end
-end)
-
-game:GetService("UserInputService").InputChanged:Connect(function(input)
-	if input == dragInput and dragging then
-		local delta = input.Position - mousePos
-		imageButton.Position = UDim2.new(
-			buttonPos.X.Scale, buttonPos.X.Offset + delta.X,
-			buttonPos.Y.Scale, buttonPos.Y.Offset + delta.Y
-		)
-	end
-end)
-
-imageButton.MouseButton1Click:Connect(function()
-	Window:Minimize()
-end)
 -- sally.sal011
