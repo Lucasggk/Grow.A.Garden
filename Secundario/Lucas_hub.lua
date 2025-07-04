386b461cc900705306d0fec15d3fd0d2ca0689ef
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-04 12:26:11 -0300

    Update Lucas_hub.lua

--- Secundario/Lucas_hub.lua
+++ Secundario/Lucas_hub.lua
@@ -1,6 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Secundario/Verifica%C3%A7%C3%A3o.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BlueLock/refs/heads/main/Fix.name.ui.lua"))()
 
+
 local sv = {
     version = "0.1", 
     patch = "Adicionando itens"
