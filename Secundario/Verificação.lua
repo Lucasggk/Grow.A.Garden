9e14b4f7a38fa37b8dd2da18760ec179df0270b5
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-04 00:13:31 -0300

    Update Verificação.lua

--- Secundario/Verificação.lua
+++ Secundario/Verificação.lua
@@ -1,3 +1,5 @@
+repeat task.wait() until game:IsLoaded()
+
 local id = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Secundario/Id.lua"))()
 local pid = game.PlaceId
 if id[pid] then
