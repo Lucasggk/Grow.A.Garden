2a8b7dfd69250fd1865830938e278dccb6891361
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-04 12:48:04 -0300

    Update Lucas_hub.lua

--- Secundario/Lucas_hub.lua
+++ Secundario/Lucas_hub.lua
@@ -1,6 +1,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Secundario/Verifica%C3%A7%C3%A3o.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BlueLock/refs/heads/main/Fix.name.ui.lua"))()
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Secundario/Functions.lua"))()
 
 local sv = {
     version = "0.1", 
@@ -19,4 +19,45 @@ local Window = Fluent:CreateWindow({
     Theme = "Dark",
     MinimizeKey = Enum.KeyCode.LeftControl
 })
+
+local home = Window:AddTab({
+        Title = "Home",
+        Icon = "user"
+    })
+
+local main = Window:AddTab({
+        Title = "main",
+        Icon = "home"
+    })
+
+local autom = Window:AddTab({
+        Title = "automatically",
+        Icon = "play"
+    })
+
+local inv = Window:AddTab({
+        Title = "inventory",
+        Icon = "bag"
+    })
+
+local shop = Window:AddTab({
+        Title = "shop",
+        Icon = "shopping-cart"
+    })
+
+local misc = Window:AddTab({
+        Title = "misc",
+        Icon = "list"
+    })
+
+local settings = Window:AddTab({
+        Title = "settings",
+        Icon = "settings"
+    })
+
+local utl = Window:AddTab({
+        Title = "utility",
+        Icon = "list"
+    })
+
 
