6ae1c9160f1ac499250aeb1eeb7b2bd8c0fd4d01
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-04 00:12:23 -0300

    Create Verificação.lua

---
+++ Secundario/Verificação.lua
@@ -0,0 +1,9 @@
+local id = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Secundario/Id.lua"))()
+local pid = game.PlaceId
+if id[pid] then
+  print("correct id place")
+else
+  return 
+end
+
+
