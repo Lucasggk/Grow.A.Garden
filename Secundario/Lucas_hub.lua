774f96b61adeb26b32815bde09447ee6fd32e392
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-04 00:26:30 -0300

    Update Lucas_hub.lua

--- Secundario/Lucas_hub.lua
+++ Secundario/Lucas_hub.lua
@@ -1,7 +1,21 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Secundario/Verifica%C3%A7%C3%A3o.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BlueLock/refs/heads/main/Fix.name.ui.lua"))()
 
-local vdu = {
-  vn = 1,
-  vp = "patch: adicionando"
+local sv = {
+    version = "0.1", 
+    patch = "Adicionando itens"
 }
+
+local vful = sv.version .." - ".. sv.patch
+
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Window = Fluent:CreateWindow({
+    Title = "Grow a Garden | ",
+    SubTitle = "Made by Lucas | ".. vful,
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, 
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
 
