99482d891cf0b61a8d7f2f175f857d2797fb0c7e
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-04 00:18:36 -0300

    Update Lucas_hub.lua

--- Secundario/Lucas_hub.lua
+++ Secundario/Lucas_hub.lua
@@ -1,1 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Secundario/Verifica%C3%A7%C3%A3o.lua"))()
+
+local vdu = {
+  vn = 1,
+  vp = "patch: adicionando"
+}
 
