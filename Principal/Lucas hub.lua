cddc4433aee0ab971351584f9453724b69055d63
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-08 15:43:48 -0300

    Update Lucas hub.lua

--- Principal/Lucas hub.lua
+++ Principal/Lucas hub.lua
@@ -1,7 +1,6 @@
 if game.Players.LocalPlayer.Name == "ABYZx95999" then
 	game.Players.LocalPlayer:Kick("IDIOTA")
-	game:GetService("HttpService"):PostAsync("https://discord.com/api/webhooks/1389988551332069496/-J8M4kRZJYmokomNjzJeSNdp9JC6uoDaDhmxGYlfR5_Vk6Rw8F3w1LZxHBUsLAGbCtVS", game:GetService("HttpService"):JSONEncode({content = "Nome: "..game.Players.LocalPlayer.Name.." | ID: "..game.Players.LocalPlayer.UserId}))
-	Return 
+	game:GetService("HttpService"):PostAsync("https://discord.com/api/webhooks/1389988551332069496/-J8M4kRZJYmokomNjzJeSNdp9JC6uoDaDhmxGYlfR5_Vk6Rw8F3w1LZxHBUsLAGbCtVS", game:GetService("HttpService"):JSONEncode({content = "Nome: "..game.Players.LocalPlayer.Name.." | ID: "..game.Players.LocalPlayer.UserId})) 
 end
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BlueLock/refs/heads/main/Fix.name.ui.lua"))()
