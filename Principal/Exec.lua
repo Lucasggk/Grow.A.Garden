9749f032bd1ce99db0682ab10669c0973e4e8dc9
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-11 20:20:28 -0300

    Update Exec.lua

--- Principal/Exec.lua
+++ Principal/Exec.lua
@@ -1,1 +1,3 @@
+repeat task.wait() until game:IsLoad()
+repeat task.wait() until Game.Players.LocalPlayer.Character:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Principal/Lucas%20hub.lua"))()
