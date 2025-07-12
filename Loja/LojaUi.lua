5a7d74a869dea2b533f1296c3595efa20524635a
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-12 17:32:29 -0300

    Update LojaUi.lua

--- Loja/LojaUi.lua
+++ Loja/LojaUi.lua
@@ -158,20 +158,42 @@ end)
 
 local section = loja:AddSection("Pets buy")
 
-loja:AddButton({
-    Title = "comprar todos pets",
-    Description = "auto se explica",
-    Callback = function()
-        buypetegg()
-    end
+local selected = {}
+
+local mdp = loja:AddDropdown("MultiDropdown", {
+    Title = "Selecionar Eggs",
+    Description = "",
+    Values = {
+        "Common Egg", "Common Summer Egg", "Rare Summer Egg",
+        "Mythical Egg", "Paradise Egg", "Bug Egg"
+    },
+    Multi = true,
+    Default = {},
 })
 
+mdp:OnChanged(function(Value)
+    selected = {}
+    for name, state in next, Value do
+        selected[#selected + 1] = name
+    end
+end)
+
+_G.bpd = false
+
 loja:AddToggle("", {
-    Title = "comprar todos pets afk",
-    Description = "auto se explica",
+    Title = "Comprar Automaticamente Eggs selecionados. \n",
+    Description = "",
     Default = false,
-    Callback = function(value)
-        bsp = value
+    Callback = function(state)
+        _G.bpd = state
+        task.spawn(function()
+            while _G.bpd do
+                for _, name in ipairs(selected) do
+                    game:GetService("ReplicatedStorage").GameEvents.BuyPetEgg:FireServer(name)
+                end
+                task.wait(1)
+            end
+        end)
     end
 })
 
@@ -179,49 +201,6 @@ local event = Window:AddTab({
     Title = "Eventos",
     Icon = "list"
 })
-
-local byallBee = { "Flower Seed Pack", "Lavender", "Nectarshade", "Nectarine", "Hive Fruit", "Pollen Radar", "Nectar Staff", "Honey Sprinkler", "Bee Egg", "Bee Crate", "Honey Comb", "Bee Chair", "Honey Torch", "Honey Walkway" }
-
-local buyBee = game:GetService("ReplicatedStorage").GameEvents.BuyEventShopStock
-local selectedBees = {}
-local bsb = false
-
-function byallbeefc()
-    for i = 1, 25 do
-        for _, bee in ipairs(selectedBees) do
-            buyBee:FireServer(bee)
-            task.wait()
-        end
-    end
-end
-
-local section = event:AddSection("Shop Honey")
-
-event:AddToggle("", {
-    Title = "Buy all Bee Shop",
-    Description = "Buy all Bee shop",
-    Default = false,
-    Callback = function(Value)
-        bsb = Value
-    end
-})
-
-local dropdownBee = event:AddDropdown("DropdownSeed", {
-    Title = "Selecione Beeshop para comprar\n",
-    Description = "Selecione Beeshop para comprar\n",
-    Values = byallBee,
-    Multi = true,
-    Default = {},
-})
-
-dropdownBee:OnChanged(function(Value)
-    selectedBees = {}
-    for beeName, selected in pairs(Value) do
-        if selected then
-            table.insert(selectedBees, beeName)
-        end
-    end
-end)
 
 task.spawn(function()
     local lastMinute = -1
@@ -246,52 +225,3 @@ task.spawn(function()
     end
 end)
 
-event:AddButton({
-    Title = "Honey Shop UI",
-    Description = "Ativa/Desativa a loja de Honey",
-    Callback = function()
-        local ui = game:GetService("Players").LocalPlayer.PlayerGui.HoneyEventShop_UI
-        if ui then
-            ui.Enabled = not ui.Enabled
-            print("Honey Shop UI:", ui.Enabled and "Ativada" or "Desativada")
-        end
-    end
-})
-
-event:AddSection("Summer")
-
-local function submitalls()
-    local args = {
-        [1] = "SubmitAllPlants"
-    }
-    game:GetService("ReplicatedStorage").GameEvents.SummerHarvestRemoteEvent:FireServer(unpack(args))
-end
-
-local tsas = 5
-event:AddSlider("Slider", {
-    Title = "Delay to submit all",
-    Description = "",
-    Default = tsas,
-    Min = 1,
-    Max = 5,
-    Rounding = 1,
-    Callback = function(v)
-        tsas = v
-    end
-})
-
-_G.AutoUsarItens = false
-
-event:AddToggle("AutoUsarItens", {
-    Title = "Auto Usar Itens",
-    Default = false,
-    Callback = function(Value)
-        _G.AutoUsarItens = Value
-        task.spawn(function()
-            while _G.AutoUsarItens do
-                submitalls()
-                task.wait(tsas)
-            end
-        end)
-    end
-})
