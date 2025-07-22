377180dbf60d0e8f45ac4d8c3de9c400a46ed5e5
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-22 17:20:57 -0300

    Update Lucas hub.lua

--- Principal/Lucas hub.lua
+++ Principal/Lucas hub.lua
@@ -828,16 +828,10 @@ local cddd = utility:AddDropdown("", {
    Values = {"Carrot", "Strawberry", "Chocolate Carrot", "Pink Tulip", "Blueberry", "Orange Tulip", "Lavender", "Stonebite", "Crocus", "Rose", "Nightshade", "Red Lollipop", "Manuka Flower", "Blue Lollipop", "Tomato", "Corn", "Daffodil", "Glowshroom", "Mint", "Cauliflower", "Bee Balm", "Peace Lily", "Horsetail", "Dandelion", "Noble Flower", "Candy Sunflower", "Pear", "Raspberry", "Watermelon", "Pumpkin", "Apple", "Bamboo", "Lingonberry", "Lilac", "Nectarine", "Violet Corn", "Cantaloupe", "Starfruit", "Moonflower", "Avocado", "Banana", "Durian", "Green Apple", "Lumira", "Peach", "Pineapple", "Coconut", "Cactus", "Dragon Fruit", "Mango", "Eggplant", "Passionfruit", "Celestiberry", "Blood Banana", "Moonglow", "Moon Melon", "Wild Carrot", "Kiwi", "Honeysuckle", "Suncoil", "Rosy Delight", "Cocovine", "Parasol Flower", "Pink Lily", "Purple Dahlia", "Firefly Fern", "Elephant Ears", "Bendboo", "Traveler's Fruit", "Amber Spine", "Boneboo", "Horned Dinoshroom", "Aloe Vera", "Cherry Blossom", "Soul Fruit", "Pepper", "Cacao", "Grape", "Cursed Fruit", "Moon Blossom", "Candy Blossom", "Lotus", "Venus Fly Trap", "Hive Fruit", "Moon Mango", "Sunflower", "Dragon Pepper", "Pitcher Plant", "Trail Fruit", "Feijoa", "Grand Volcania", "Fossilight", "Sugar Apple", "Ember Lily", "Burning Bud", "Giant Pinecone", "Beanstalk", "Bone Blossom"},
    Multi = true,
    Default = {},
+   Callback = function(v)
+	FrutasToColl = v
+   end
 })
-
-cddd:OnChanged(function(Value)
-    FrutasToColl = {}
-    for v, selected in next, Value do
-        if selected then
-            table.insert(FrutasToColl, v)
-        end
-    end
-end)
 
 local function cfvv(nome)
     for _, planta in ipairs(workspace.Farm.Farm.Important.Plants_Physical:GetChildren()) do
