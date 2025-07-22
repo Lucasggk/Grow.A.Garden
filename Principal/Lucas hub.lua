f47eab21310caf151a1c75bc24a1f409191e8dc9
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-22 17:41:37 -0300

    Update Lucas hub.lua

--- Principal/Lucas hub.lua
+++ Principal/Lucas hub.lua
@@ -2,7 +2,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BlueLock/ref
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Principal/Webhook%20De%20ideias.lua"))()
 local script_version = {
     -- version
-    version = "2.59[Auto Collect[3]",
+    version = "2.59[Auto Collect[4]",
     alpha = true,
 }
 if script_version.alpha then
