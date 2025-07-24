f4a93edc6c5abf8ce7cfa82125fb1a8cfcc11133
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-24 18:06:11 -0300

    Update Lucas hub.lua

--- Principal/Lucas hub.lua
+++ Principal/Lucas hub.lua
@@ -2,7 +2,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BlueLock/ref
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Grow.A.Garden/refs/heads/main/Principal/Webhook%20De%20ideias.lua"))()
 local script_version = {
     -- version
-    version = "2.8[Zen event Progress: shop..🌙 4]",
+    version = "2.8[Zen event Progress: shop..🌙 5]",
     alpha = true,
 }
 if script_version.alpha then
