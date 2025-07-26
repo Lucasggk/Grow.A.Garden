c5f104efb97a2c3f7f0e8acbdc61405ed4193f48
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-25 22:21:47 -0300

    Update Lucas hub.lua

--- Principal/Lucas hub.lua
+++ Principal/Lucas hub.lua
@@ -907,7 +907,7 @@ end
 
 _G.cfm = false
 local mtcc
-local mtc = {"None", "Wet", "Windstruck", "Moonlit", "Chilled", "Shocked", "Frozen", "Bloodlit", "Celestial", "Zombified", "Honey Glazed", "Heavenly", "Fried", "Amber", "Clay", "Ceramic", "Sundried", "Aurora", "Alienlike", "Galactic", "Disco", "Plasma", "Tranquil", "Cooked", "Burnt", "Sandy", "Pollinated", "Friendbound", "Radioactive", "Foxfire Chakra", "Twisted", "Molten", "Meteoric", "Voidtouched", "Dawnbound"}
+local mtc = {"None", "Wet", "Windstruck", "Moonlit", "Chilled", "Shocked", "Frozen", "Bloodlit", "Celestial", "Zombified", "HoneyGlazed", "Heavenly", "Fried", "Amber", "Clay", "Ceramic", "Sundried", "Aurora", "Alienlike", "Galactic", "Disco", "Plasma", "Tranquil", "Cooked", "Burnt", "Sandy", "Pollinated", "Friendbound", "Radioactive", "Foxfire Chakra", "Twisted", "Molten", "Meteoric", "Voidtouched", "Dawnbound"}
 
 utility:AddDropdown("", {
     Title = "Selecione a Mutação.",
